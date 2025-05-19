f12be9e8e144808e1d58c99869803a009586a4ed
Jonathan Alphabert <jonathanalphabert0504@gmail.com>
2025-05-19 14:33:16 +0700

    upload image and design ui

--- lib/presentation/SPK/spk_checklist_screen.dart
+++ lib/presentation/SPK/spk_checklist_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
 import 'dart:math';
 
+import 'package:fdpi_app/presentation/widgets/spk/spk_category.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -147,6 +149,7 @@ class _NewSpkChecklistScreenState extends State<NewSpkChecklistScreen> {
                             ),
                         body: Column(
                           children: [
+                            // Table header
                             Container(
                               width: double.infinity,
                               decoration: const BoxDecoration(
@@ -191,6 +194,198 @@ class _NewSpkChecklistScreenState extends State<NewSpkChecklistScreen> {
                                 ],
                               ),
                             ),
+
+                            ...item['data']
+                                .asMap()
+                                .entries
+                                .map<Widget>(
+                                  (category) => SpkChecklistAccordion(
+                                    index: category.key,
+                                    showCheckboxQC: false,
+                                    showCheckboxDone: false,
+                                    initiallyExpanded: true,
+                                    backgroundColor: const Color(0xFFC7C7C7),
+                                    title: category.value['category'],
+                                    content: Container(
+                                      child: Column(
+                                        children: [
+                                          ...category.value['data']
+                                              .asMap()
+                                              .entries
+                                              .map(
+                                                (
+                                                  qcItem,
+                                                ) => SpkChecklistAccordion(
+                                                  index: qcItem.key,
+                                                  showCheckboxQC: true,
+                                                  showCheckboxDone: true,
+                                                  showIcon: false,
+                                                  title:
+                                                      qcItem.value['qc_item'],
+                                                  content: Container(
+                                                    width: double.infinity,
+                                                    padding: EdgeInsets.all(
+                                                      16.w,
+                                                    ),
+                                                    child: Column(
+                                                      crossAxisAlignment:
+                                                          CrossAxisAlignment
+                                                              .start,
+                                                      children: [
+                                                        Text(
+                                                          "Remark",
+                                                          style: TextStyle(
+                                                            fontWeight:
+                                                                FontWeight.bold,
+                                                          ),
+                                                        ),
+                                                        SizedBox(height: 4.w),
+                                                        Text(
+                                                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                                                          maxLines: 2,
+                                                          overflow:
+                                                              TextOverflow
+                                                                  .ellipsis,
+                                                        ),
+                                                        SizedBox(height: 8.w),
+
+                                                        Text(
+                                                          "Attachment",
+                                                          style: TextStyle(
+                                                            fontWeight:
+                                                                FontWeight.bold,
+                                                          ),
+                                                        ),
+                                                        SizedBox(height: 4.w),
+                                                        SingleChildScrollView(
+                                                          scrollDirection:
+                                                              Axis.horizontal,
+                                                          child: Row(
+                                                            children: [
+                                                              ...List.generate(
+                                                                Random().nextInt(
+                                                                      6,
+                                                                    ) +
+                                                                    1,
+                                                                (
+                                                                  index,
+                                                                ) => Padding(
+                                                                  padding:
+                                                                      EdgeInsets.only(
+                                                                        right:
+                                                                            4.w,
+                                                                      ),
+                                                                  child: SizedBox(
+                                                                    width: 64.w,
+                                                                    height:
+                                                                        64.w,
+                                                                    child: Container(
+                                                                      decoration: BoxDecoration(
+                                                                        borderRadius:
+                                                                            BorderRadius.circular(
+                                                                              2,
+                                                                            ),
+                                                                        color:
+                                                                            Colors.grey[300],
+                                                                      ),
+                                                                    ),
+                                                                  ),
+                                                                ),
+                                                              ),
+                                                              GestureDetector(
+                                                                onTap: () async {
+                                                                  FilePickerResult?
+                                                                  result = await FilePicker
+                                                                      .platform
+                                                                      .pickFiles(
+                                                                        allowMultiple:
+                                                                            true,
+                                                                      );
+
+                                                                  if (result !=
+                                                                      null) {
+                                                                    List<File>
+                                                                    files =
+                                                                        result
+                                                                            .paths
+                                                                            .map(
+                                                                              (
+                                                                                path,
+                                                                              ) => File(
+                                                                                path!,
+                                                                              ),
+                                                                            )
+                                                                            .toList();
+                                                                  } else {
+                                                                    // User canceled the picker
+                                                                  }
+                                                                },
+                                                                child: SizedBox(
+                                                                  width: 64.w,
+                                                                  height: 64.w,
+                                                                  child: Container(
+                                                                    decoration: BoxDecoration(
+                                                                      border: Border.all(
+                                                                        color: Color(
+                                                                          0xffE2E2E2,
+                                                                        ),
+                                                                      ),
+                                                                      borderRadius:
+                                                                          BorderRadius.circular(
+                                                                            2,
+                                                                          ),
+                                                                      color: Color(
+                                                                        0xffF9F9F9,
+                                                                      ),
+                                                                    ),
+                                                                    child: Column(
+                                                                      mainAxisAlignment:
+                                                                          MainAxisAlignment
+                                                                              .center,
+                                                                      children: [
+                                                                        Icon(
+                                                                          Icons
+                                                                              .upload,
+                                                                          color: Color(
+                                                                            0xff555555,
+                                                                          ),
+                                                                          size:
+                                                                              16.w,
+                                                                        ),
+                                                                        SizedBox(
+                                                                          height:
+                                                                              4.w,
+                                                                        ),
+                                                                        Text(
+                                                                          "Upload",
+                                                                          style: TextStyle(
+                                                                            color: Color(
+                                                                              0xff555555,
+                                                                            ),
+                                                                            fontSize:
+                                                                                10.sp,
+                                                                          ),
+                                                                        ),
+                                                                      ],
+                                                                    ),
+                                                                  ),
+                                                                ),
+                                                              ),
+                                                            ],
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  ),
+                                                ),
+                                              )
+                                              .toList(),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                )
+                                .toList(),
                           ],
                         ),
                       );
